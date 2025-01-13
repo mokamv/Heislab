98aa25062f73880f3a2545ca065649e169df44b7
Léo Bernard <leo.bernard0811@gmail.com>
2025-01-13 11:39:39 +0100

    New: Listening to broadcast

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::io;
 use std::io::prelude::*;
 use std::net::TcpStream;
-use std::ops::Add;
 use std::thread::sleep;
 use std::time::Duration;
+use std::net::UdpSocket;
 
-fn main() -> std::io::Result<()> {
+fn main() {
+    //tcp();
+    udp();
+}
+
+fn tcp() -> io::Result<()> {
     let ip = "10.24.150.137:34933";
     let mut read_buffer = [0; 1024];
 
@@ -27,7 +32,6 @@ fn main() -> std::io::Result<()> {
 
         let mut to_send = String::from(to_send.trim());
 
-
         to_send.push('\0');
 
         stream.write(&to_send.as_bytes()).expect("An error occured");
@@ -35,4 +39,21 @@ fn main() -> std::io::Result<()> {
         stream.read(&mut read_buffer).expect("An error occured on read");
         println!("{}", String::from_utf8_lossy(&read_buffer));
     };
-} // the stream is closed here
+}
+
+fn udp() -> io::Result<()> {
+    // Create a UDP socket
+    let socket = UdpSocket::bind("0.0.0.0:30000")?;
+
+    loop {
+        // Create a buffer to receive data
+        let mut buf = [0; 1024];
+
+        // Receive data from the socket
+        let (len, src) = socket.recv_from(&mut buf)?;
+
+        // Print the received data and source address
+        println!("Received {} bytes from {:?}", len, src);
+        println!("Data: {:?}", &buf[..len]);
+    }
+}
