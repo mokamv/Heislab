63912286d73f7f76fc6611b0420d394d2ca7042d
Sara Ostnor <saraao@stud.ntnu.no>
2025-02-24 12:06:29 +0100

    hei

---
+++ cost/src/cost.rs
@@ -0,0 +1,18 @@
+use Heislab::common:src::messages 
+use Heislab::src::single_elevator_controller
+
+//todo: 
+// change to not single elevator
+
+fun cost(message : Message) {
+
+    let mut weight = 0;
+    let State:Elevator elevatorManageCall;
+    for n in ids{
+        let mut weightElevatorN = 0;
+        if weight > weightElevatorN:
+            elevatorManageCall = elevator n
+    }
+    ElevatorState->queueN.add_call(massage[])
+    
+}
