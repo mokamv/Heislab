bbc0706ea1282ec0c4048c98965ae9f6d2e1f7a1
Léo Bernard <leo.bernard0811@gmail.com>
2025-02-24 10:10:20 +0100

    Add messages

--- common/src/messages.rs
+++ common/src/messages.rs
@@ -1,18 +1,27 @@
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
-use crate::messages::Message::{ClientAuth, KeepAlive, SocketAddress};
+use crate::messages::Message::{ClientAuth, ClientButtonCall, ClientObstructed, ClientReachFloor, GotoFloor, KeepAlive, LightControl, SocketAddress};
 
 pub type RawMessage = [u8; MESSAGE_SIZE];
 pub const MESSAGE_SIZE: usize = 32;
 pub const DEFAULT_MESSAGE: [u8; MESSAGE_SIZE] = [0u8; MESSAGE_SIZE];
 pub use DEFAULT_MESSAGE as KEEP_ALIVE_MESSAGE;
-
+use crate::messages::PhysicalButton::{CAB, HALL};
 
 // Limited to 255 types of messages if coding message type on the first byte
 #[derive(Debug)]
 pub enum Message {
     KeepAlive,
-    SocketAddress{ address: SocketAddr },
+
+    // Client messages
     ClientAuth{ client_id: u8 }, // Client identifier
+    ClientObstructed { is_obstructed: bool },
+    ClientReachFloor { floor_reached: u8 },
+    ClientButtonCall { pressed: PhysicalButton },
+
+    // Controller messages
+    SocketAddress{ address: SocketAddr },
+    LightControl { target: PhysicalButton, is_lit: bool },
+    GotoFloor { go_to_floor: u8 }
 }
 
 impl Message {
@@ -25,8 +34,29 @@ impl Message {
         let mut raw_message = [0u8; MESSAGE_SIZE];
         match self {
             KeepAlive => {}
-            SocketAddress { address } => {
+
+            // Client encode
+            ClientAuth { client_id } => {
                 raw_message[0] = 1;
+                raw_message[1] = client_id
+            }
+            ClientObstructed { is_obstructed } => {
+                raw_message[0] = 2;
+                raw_message[1] = is_obstructed as u8
+
+            },
+            ClientReachFloor { floor_reached } => {
+                raw_message[0] = 3;
+                raw_message[1] = floor_reached
+            },
+            ClientButtonCall { pressed } => {
+                raw_message[0] = 4;
+                raw_message[1..4].copy_from_slice(&pressed.encode());
+            },
+
+            // Controller encode
+            SocketAddress { address } => {
+                raw_message[0] = 128;
                 match address.ip() {
                     IpAddr::V4(ipv4) => {
                         raw_message[1] = 4;
@@ -39,11 +69,16 @@ impl Message {
                         raw_message[18..18 + size_of::<u16>()].copy_from_slice(&address.port().to_be_bytes())
                     }
                 }
-            }
-            ClientAuth { client_id } => {
-                raw_message[0] = 2;
-                raw_message[1] = client_id
-            }
+            },
+            LightControl { target, is_lit } => {
+                raw_message[0] = 129;
+                raw_message[1..4].copy_from_slice(&target.encode());
+                raw_message[4] = is_lit as u8;
+            },
+            GotoFloor { go_to_floor } => {
+                raw_message[0] = 130;
+                raw_message[1] = go_to_floor;
+            },
         }
 
         raw_message
@@ -51,7 +86,14 @@ impl Message {
     pub fn decode_message(raw_message: &RawMessage) -> Self {
         match raw_message[0] {
             0 => KeepAlive,
-            1 => SocketAddress { address: match raw_message[1] {
+            1 => ClientAuth { client_id: raw_message[1] },
+            2 => ClientObstructed { is_obstructed: raw_message[1] != 0 },
+            3 => ClientReachFloor { floor_reached: raw_message[1] },
+            4 => ClientButtonCall { pressed: PhysicalButton::decode(&raw_message[1..4]) },
+
+
+
+            128 => SocketAddress { address: match raw_message[1] {
                     4 => {
                         let mut ip = [0u8; 4]; let mut port = [0u8; 2];
                         ip.copy_from_slice(&raw_message[2..6]);
@@ -66,8 +108,43 @@ impl Message {
                     }
                     _ => panic!("TODO")
                 }, },
-            2 => ClientAuth { client_id: raw_message[1] },
+            129 => LightControl { target: PhysicalButton::decode(&raw_message[1..4]), is_lit: raw_message[4] != 0 },
+            130 => GotoFloor { go_to_floor: raw_message[1] },
             _ => panic!("TODO"),
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub enum PhysicalButton {
+    HALL { floor: u8, direction_is_up: bool },
+    CAB { floor: u8 }
+}
+
+impl PhysicalButton {
+    fn encode(&self) -> [u8; 3] {
+        let mut message = [0u8; 3];
+        match self {
+            HALL { floor, direction_is_up } => {
+                message[0] = 0;
+                message[1] = *floor;
+                message[2] = *direction_is_up as u8;
+            }
+            CAB { floor } => {
+                message[0] = 1;
+                message[1] = *floor;
+
+            }
+        };
+        message
+    }
+
+    fn decode(raw_button: &[u8]) -> Self {
+        assert_eq!(raw_button.len(), 3);
+        match raw_button[0] {
+            0 => HALL { floor: raw_button[1], direction_is_up: raw_button[2] != 0 },
+            _ => CAB { floor: raw_button[1] }
         }
     }
 }
