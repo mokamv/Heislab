d122e6a73a18d2800084103dfde54326d6ce9962
mariusrena <mariusrena@outlook.com>
2025-03-27 22:06:18 +0100

    mi

--- process/src/elevator/controller/elevator_pool.rs
+++ process/src/elevator/controller/elevator_pool.rs
@@ -137,6 +137,8 @@ impl ElevatorPool {
         match cabin_state {
             CabinState::Idle { .. } => {
                 let next_command = elevator.get_next_command();
+                println!("Next command: ");
+                println!("{next_command:?}");
                 if let Some(next_command) = next_command {
                     controller_handle.send_client_message(
                         Target::Specific(elevator_id),
